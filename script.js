445fb7f43f282e8006af5857bc9ebb67b8f81a5e
vehN <bbeddell@gmail.com>
2025-03-14 11:39:22 +0800

    asset changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("June 2 2025 00:00:00").getTime();
+const targetDate = new Date("March 17 2025 00:00:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
